a0eb23f54f5dbe424b50d32192876b8a8c654aab
EMcKee10 <ethanmckee10@yahoo.com>
2019-06-03 11:03:31 -0500

    Fix issue #1-6, compartmentalize logic, add console functionality

--- src/main/java/io/github/emckee10/specialreservation/SRExecutor.java
+++ src/main/java/io/github/emckee10/specialreservation/SRExecutor.java
@@ -17,79 +17,127 @@ public class SRExecutor implements CommandExecutor
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
   {
-    if (command.getName().equalsIgnoreCase("NumOfReservedSlots")) {
-      if (args.length == 0 || args.length > 1) {
-        //sender.sendMessage(SRUtil.color(sr.getConfig().getString("commands.numofreservedslots.usage")));
-        return false;
-      }
-      else {
-        if (!SRUtil.isInteger(args[0])) {
-          //sender.sendMessage(SRUtil.color(sr.getConfig().getString("commands.numofreservedslots.usage")));
-          return false;
-        }
-        else {
-          if (SRUtil.parseInteger(args[0]) <= sr.getMaxPlayers()) {
-            if (SRUtil.parseInteger(args[0]) == sr.getMaxPlayers()) {
-              sender.sendMessage(SRUtil.color("&cWarning: &eNumber of reserved slots equals the capacity therefor no regular players will be able to get in."));
-            }
-    
-            sr.setSlots(SRUtil.parseInteger(args[0]));
-            sr.setLocalSlots(sr.getSlots());
-            sender.sendMessage(SRUtil.color("&l&3 Success! There are now " + args[0] + " out of " + sr.getMaxPlayers() + " reserved slots on the server!"));
-  
-            //TODO if staff toggle is off then count these staff in warning
-            if (sr.getRegularPlayers() > 0 || sr.getSpecialPlayers() > 0) {
-              sender.sendMessage(SRUtil.color("&cWarning: &eServer is not empty, this could effect the way the [Special_Reservations] plugin works."));
-            }
-            return true;
-          }
-          else {
-            sender.sendMessage(SRUtil.color("&cInvalid option, " + args[0] + " is greater than the capacity. capacity has been set to " + sr.getMaxPlayers()));
-            return true;
-          }
-        }
-      }
+    boolean result;
+    switch (command.getName().toLowerCase())
+    {
+      case "reserved":
+        result = changeReservedNum(sender, args);
+        break;
+      case "rstoggle":
+        result = rstoggle(sender, args);
+        break;
+      case "changefullmessage":
+        result = changeFullMessage(sender, args);
+        break;
+      default:
+        result = false;
     }
-    else if (command.getName().equalsIgnoreCase("ReserveSlotsToggle")) {
-      if (args.length == 0) {
-        //sender.sendMessage(SRUtil.color(sr.getConfig().getString("commands.reserveslotstoggle.usage")));
-        return false;
+    return result;
+  }
+
+  private boolean changeFullMessage(CommandSender sender, String[] args)
+  {
+    if (args.length == 0)
+    {
+      sender.sendMessage(SRUtil.color("&cInvalid entry. You must include a message with this command"));
+      return false;
+    } else
+    {
+      StringBuilder builder = new StringBuilder();
+      for (String arg : args)
+      {
+        builder.append(" ").append(arg);
       }
-      else {
-        if (args[0].equalsIgnoreCase("true")) {
-          if (!sr.Toggle) {
+      String message = builder.toString().trim();
+      sr.setMessage(message);
+      sender.sendMessage(SRUtil.color("&2Success! The message you display when full is: " + message));
+      return true;
+    }
+  }
+
+  private boolean rstoggle(CommandSender sender, String[] args)
+  {
+    if (args.length == 0)
+    {
+      sender.sendMessage(SRUtil.color("&cIncorrect command usage, try /ReserveSlotsToggle <true/false>."));
+      return true;
+    } else
+    {
+      if (args[0].equalsIgnoreCase("true"))
+      {
+        if (!sr.Toggle)
+        {
+          if (sr.getRegularPlayers() > 0 || sr.getSpecialPlayers() > 0)
+          {
+            sender.sendMessage(SRUtil.color("&cServer is not empty (op/staff not included in this), clear all regular and special players before turning [Special Reservations] on"));
+          } else
+          {
             sr.setToggle(true);
+            sender.sendMessage(SRUtil.color("&2Reserved Slots is now turned on"));
+          }
+        } else
+        {
+          sender.sendMessage(SRUtil.color("&eReserved Slots is already on"));
+        }
+        return true;
+      } else if (args[0].equalsIgnoreCase("false"))
+      {
+        if (sr.Toggle)
+        {
+          sr.setToggle(false);
+          sender.sendMessage(SRUtil.color("&2Reserved Slots is now turned off"));
+        } else
+        {
+          sender.sendMessage(SRUtil.color("&eReserved Slots is already off"));
+        }
+        return true;
+      } else
+      {
+        sender.sendMessage(SRUtil.color("&cIncorrect command usage, try /ReserveSlotsToggle <true/false>."));
+        return true;
+      }
+    }
+  }
+
+  private boolean changeReservedNum(CommandSender sender, String[] args)
+  {
+
+    if (args.length == 0 || args.length > 1)
+    {
+      sender.sendMessage(SRUtil.color("&cUsage: /<NumOfReservedSlots> [Integer of reserved slots for the server]"));
+      return true;
+    } else
+    {
+      if (!SRUtil.isInteger(args[0]))
+      {
+        sender.sendMessage(SRUtil.color("&cUsage: /<NumOfReservedSlots> [Integer of reserved slots for the server]"));
+        return true;
+      } else
+      {
+        if (SRUtil.parseInteger(args[0]) <= sr.getMaxPlayers())
+        {
+          if (SRUtil.parseInteger(args[0]) == sr.getMaxPlayers())
+          {
+            sender.sendMessage(SRUtil.color("&cWarning: &eNumber of reserved slots equals the capacity, therefore no regular players will be able to get in."));
+          }
+
+          sr.setSlots(SRUtil.parseInteger(args[0]));
+          sr.setLocalSlots(sr.getSlots());
+          sender.sendMessage(SRUtil.color("&l&2 Success! There are now " + args[0] + " out of " + sr.getMaxPlayers() + " reserved slots on the server!"));
+
+          //TODO if staff toggle is off then count these staff in warning
+          if (sr.getRegularPlayers() > 0 || sr.getSpecialPlayers() > 0)
+          {
+            sender.sendMessage(SRUtil.color("&cWarning: &eServer is not empty, this could effect the way the [Special_Reservations] plugin works."));
           }
           return true;
-        }
-        else if (args[0].equalsIgnoreCase("false")) {
-          if (sr.Toggle) {
-            sr.setToggle(false);
-          }
+        } else
+        {
+          sender.sendMessage(SRUtil.color("&cInvalid option. " + args[0] + " is greater than the capacity. Capacity is set to " + sr.getMaxPlayers()));
           return true;
-        }
-        else {
-          //sender.sendMessage(SRUtil.color(sr.getConfig().getString("commands.reserveslotstoggle.usage")));
-          return false;
         }
       }
     }
-    else if (command.getName().equalsIgnoreCase("changeFullMessage")) {
-      if (args.length == 0) {
-        sender.sendMessage(SRUtil.color("&cInvalid entry, you must include a message with this command"));
-        return false;
-      }
-      else {
-        StringBuilder builder = new StringBuilder();
-        for (String arg : args) {
-          builder.append(" ").append(arg);
-        }
-        String message = builder.toString().trim();
-        sr.setMessage(message);
-      }
-    }
-    return false;
+
   }
 }
-
-
--- src/main/java/io/github/emckee10/specialreservation/SRListener.java
+++ src/main/java/io/github/emckee10/specialreservation/SRListener.java
@@ -14,7 +14,7 @@ public class SRListener implements Listener
   {
     this.sr = sr;
   }
-  
+
   public static boolean hasSpecialPermission(Player player)
   {
     return player.hasPermission("special_reservation.specialbypass");
@@ -30,49 +30,58 @@ public class SRListener implements Listener
         if (hasStaffPermission(event.getPlayer()))
         {
           event.allow();
+          System.out.println("Staff player entered");
           //TODO add staff toggle switch
         } else if (hasSpecialPermission(event.getPlayer()) && sr.getSpecialPlayers() >= sr.getSlots() && sr.getRegularPlayers() < getRegularPlayerSlots())
         {
+          System.out.println("Special player entered regular slot");
           sr.setRegularPlayers(sr.getRegularPlayers() + 1);
           event.allow();
         } else if (hasSpecialPermission(event.getPlayer()) && sr.getSpecialPlayers() < sr.getSlots())
         {
+          System.out.println("Special player entered special slot");
           sr.setSpecialPlayers(sr.getSpecialPlayers() + 1);
           event.allow();
         } else if (!hasSpecialPermission(event.getPlayer()) && !hasStaffPermission(event.getPlayer()) && sr.getRegularPlayers() < getRegularPlayerSlots())
         {
+          System.out.println("Regular player entered regular slot");
           sr.setRegularPlayers(sr.getRegularPlayers() + 1);
           event.allow();
         } else
         {
-          event.disallow(PlayerLoginEvent.Result.KICK_OTHER, sr.getMessage());
+          event.disallow(PlayerLoginEvent.Result.KICK_OTHER, SRUtil.color(sr.getMessage()));
         }
       }
     }
   }
-  
+
+  private int getRegularPlayerSlots()
+  {
+    return sr.getMaxPlayers() - sr.getLocalSlots();
+  }
+
   @EventHandler
   public void onLogout(PlayerQuitEvent event)
   {
     if (sr.isToggle()) {
       if (hasStaffPermission(event.getPlayer())) {
+        System.out.println("Staff player exited");
         //TODO add staff toggle switch
       }
       else if (hasSpecialPermission(event.getPlayer()) && sr.getSpecialPlayers() > 0) {
+        System.out.println("Special player left special slot");
         sr.setSpecialPlayers(sr.getSpecialPlayers() - 1);
       }
       else if (hasSpecialPermission(event.getPlayer()) && sr.getSpecialPlayers() <= 0) {
+        System.out.println("Special player left regular slot");
         sr.setRegularPlayers(sr.getRegularPlayers() - 1);
       }
       else if (!hasSpecialPermission(event.getPlayer()) && !hasStaffPermission(event.getPlayer())) {
+        System.out.println("Regular player left regular slot");
         sr.setRegularPlayers(sr.getRegularPlayers() - 1);
+
       }
     }
-  }
-  
-  private int getRegularPlayerSlots()
-  {
-    return sr.getMaxPlayers() - sr.getLocalSlots();
   }
   
   private boolean hasStaffPermission(Player player)
